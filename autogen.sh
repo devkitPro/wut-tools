b0b5b5d6c70a12e54b0c2730ba50b11a1d354ccd
Dave Murphy <davem@devkitpro.org>
2019-01-14 19:38:51 +0000

    build libfmt as local lib

--- autogen.sh
+++ autogen.sh
@@ -1,3 +1,12 @@
-aclocal
-autoconf
-automake --add-missing -c
+#!/bin/sh
+if test ! "x$(which libtoolize)" = "x"; then
+  echo "Running libtoolize"
+  libtoolize --copy --force --automake
+else
+  if test ! "x$(which gintltoolize)" = "x"; then
+    echo "Running glibtoolize"
+    glibtoolize --copy --force --automake
+  fi
+fi
+autoreconf --install --force --verbose
+
